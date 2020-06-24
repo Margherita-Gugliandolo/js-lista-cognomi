0d056ed726e78efe785dccf2a07bc858c26727a5
Margherita <mangoguglia@gmail.com>
2020-06-24 16:56:40 +0200

    lista-cognomi-js
    con for

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ var listaCognomi = ["Bianchi", "Rossi", "Monti", "Crozza", "Bisio"];
 var cognomeUtente = prompt("Scrivi un cognome");
 
 listaCognomi.push(cognomeUtente);
-listaCognomi.sort();
+//listaCognomi.sort();
+
+for (var i = 0; i < listaCognomi.length - 1 ; i++) {
+  console.log(listaCognomi[i].localeCompare(listaCognomi[i+1]));
+}
 
 console.log(listaCognomi);
 
